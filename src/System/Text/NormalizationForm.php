ffb4f776400239a21a8e1d547c083fe0145c66fa
Alexandre Barbieri <fakeezz@gmail.com>
2012-11-16 15:27:10 -0200

    added NormalizationForm

---
+++ src/System/Text/NormalizationForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace System\Text {
+
+    use \System\Enum as Enum;
+
+    /**
+     * Defines the type of normalization to perform.
+     * @access public
+     * @package System
+     * @subpackage Text
+     * @name NormalizationForm
+     */
+    class NormalizationForm extends Enum {
+
+        private function __construct($name, $value) {
+            $this->name = $name;
+            $this->value = $value;
+        }
+
+        /**
+         * Indicates that a Unicode string is normalized using full canonical decomposition, followed by the replacement of sequences with their primary composites, if possible. 
+         * @access public
+         * @return \System\Text\NormalizationForm
+         */
+        public static function formC() {
+            return new DayOfWeek("FormC", 0);
+        }
+
+        /**
+         * Indicates that a Unicode string is normalized using full canonical decomposition. 
+         * @access public
+         * @return \System\Text\NormalizationForm
+         */
+        public static function formD() {
+            return new DayOfWeek("FormD", 1);
+        }
+
+        /**
+         * Indicates that a Unicode string is normalized using full compatibility decomposition, followed by the replacement of sequences with their primary composites, if possible. 
+         * @access public
+         * @return \System\Text\NormalizationForm
+         */
+        public static function formKC() {
+            return new DayOfWeek("FormKC", 2);
+        }
+
+        /**
+         * Indicates that a Unicode string is normalized using full compatibility decomposition. 
+         * @access public
+         * @return \System\Text\NormalizationForm
+         */
+        public static function formKD() {
+            return new DayOfWeek("FormKD", 3);
+        }
+
+    }
+}
