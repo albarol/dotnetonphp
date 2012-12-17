31f36c5e9e5f5f34cb43e9be50b9d81a9dc11f3c
Alexandre Barbieri <fakeezz@gmail.com>
2012-12-17 10:53:12 -0200

    refactored tests to \System\Random

--- src/System/Random.php
+++ src/System/Random.php
@@ -2,7 +2,6 @@
 
 namespace System {
 
-    use \System\Int32 as Int32;
     use \System\ArgumentOutOfRangeException as ArgumentOutOfRangeException;
     use \System\ArgumentNullException as ArgumentNullException;
 
@@ -16,26 +15,27 @@ namespace System {
 
         private $seed;
 
-        public function __construct($seed=Int32::MAX_VALUE) {
-            $this->seed;
+        public function __construct($seed=2147483647) {
+            $this->seed = $seed;
         }
 
         /**
          * Returns a nonnegative random number.
          * @access public
+         * @throws \System\ArgumentOutOfRangeException maxValue is less than zero. -or- max value is greater than \System\Int32::MAX_VALUE -or- min value is greater than max value.
          * @param int $min The inclusive lower bound of the random number returned.
          * @param int $max The exclusive upper bound of the random number returned. maxValue must be greater than or equal to minValue.
          * @return int A 32-bit signed integer greater than or equal to minValue and less than maxValue; that is, the range of return values includes minValue but not maxValue. If minValue equals maxValue, minValue is returned.
          */
-        public function next($min=0, $max=0) {
-            if($min == 0 && $max == 0) {
-                $this->validateNumber(0, $this->seed);
-                return rand(0, $this->seed);
-            }
-            if($max == 0) {
-                $this->validateNumber(0, $min);
-                return rand(0, $min);
-            }
+        public function next($min = null, $max = null) {
+            if(is_null($min) && is_null($max)):
+                $min = 0;
+                $max = $this->seed;
+            elseif (is_null($max)):
+                $max = $min;
+                $min = 0;
+            endif;
+
             $this->validateNumber($min, $max);
             return rand($min, $max);
         }
@@ -53,18 +53,26 @@ namespace System {
         /**
          * Fills the elements of a specified array of bytes with random numbers.
          * @access public
+         * @throws \System\ArgumentNullException buffer is null.
          * @param $buffer An array of bytes to contain random numbers.
          */
         public function nextBytes(&$buffer) {
-            if($buffer == null) throw new ArgumentNullException("buffer is null.");
-            for($i = 0; $i < sizeof($buffer); $i++)
+            if($buffer == null):
+                throw new ArgumentNullException("buffer is null.");
+            endif;
+            for($i = 0; $i < sizeof($buffer); $i++):
                 $buffer[$i] = $this->next(0, 255);
+            endfor;
         }
 
         private function validateNumber($min, $max) {
-            if($max < 0) throw new ArgumentOutOfRangeException("maxValue is less than zero.");
-            if($max > Int32::MaxValue) throw new ArgumentOutOfRangeException("max value is greater than System.Int32.MaxValue");
-            if($min > $max) throw new ArgumentOutOfRangeException("minValue is greater than maxValue.");
+            if ($max < 0):
+                throw new ArgumentOutOfRangeException("max value is less than zero.");
+            elseif ($max > 2147483647):
+                throw new ArgumentOutOfRangeException("max value is greater than \System\Int32::MAX_VALUE");
+            elseif($min > $max):
+                throw new ArgumentOutOfRangeException("min value is greater than max value.");
+            endif;
         }
     }
 }
--- tests/System/RandomFixture.php
+++ tests/System/RandomFixture.php
@@ -1,77 +1,119 @@
 <?php
 
-//require_once 'PHPUnit/Framework.php';
+require_once dirname(__FILE__) . '/../../src/Autoloader.php';
 
-require_once dirname(__FILE__) . '/../../system/Random.php';
+use \System\Random as Random;
 
 class RandomTest extends PHPUnit_Framework_TestCase {
 
-    public function testRandomNumber() {
+    public function test_Next_CanGenerateRandomNumber() {
+        # Arrange:
         $rnd = new Random();
+        
+        # Act:
         $number = $rnd->next();
+        
+        # Assert:
         $this->assertGreaterThanOrEqual(0, $number);
     }
 
-    public function testRandomNumberWithSeed() {
+    public function test_Next_CanGenerateRandomNumberWithSeed() {
+        # Arrange:
         $rnd = new Random(10);
+               
+        # Act:
         $number = $rnd->next();
+        
+        # Assert:
         $this->assertGreaterThanOrEqual(0, $number);
         $this->assertLessThanOrEqual(10, $number);
     }
 
-    public function testMaxRandomNumber() {
+
+    public function test_Next_GenerateRandomShouldBeLessThanTen() {
+        # Arrange:
         $rnd = new Random();
-        $number = $rnd->next(10);
-        $this->assertLessThanOrEqual(10, $number);
+        $max_number = 10;
+        
+        # Act:
+        $number = $rnd->next($max_number);
+        
+        # Assert:
+        $this->assertLessThanOrEqual($max_number, $number);
     }
 
-    public function testCantGetMaxRandomNumberBecauseMaxValueIsLessThanZero() {
-        $this->setExpectedException("ArgumentOutOfRangeException");
+    public function test_Next_ThrowsExceptionWhenMinIsLessThanZero() {
+        # Arrange:
+        $this->setExpectedException("\\System\\ArgumentOutOfRangeException");
         $rnd = new Random();
+        
+        # Act:
         $number = $rnd->next(-1);
     }
 
-    public function testCantGetMaxRandomNumberBecauseMaxValueGreaterThanInt32MaxValue() {
-        $this->setExpectedException("ArgumentOutOfRangeException");
+    public function test_Next_ThrowsExceptionWhenMinIsGreaterThanMaxValue() { 
+        # Arrange:
+        $this->setExpectedException("\\System\\ArgumentOutOfRangeException");
         $rnd = new Random();
-        $number = $rnd->next(Int32::MaxValue+1);
+
+        # Act:
+        $number = $rnd->next(2147483648);
     }
 
-    public function testMinAndMaxRandomNumber() {
+    public function test_Next_CanGenerateNumberBetweenMinAndMax() {
+        # Arrange:
         $rnd = new Random();
+        
+        # Act:
         $number = $rnd->next(5, 10);
+        
+        # Assert:
         $this->assertGreaterThanOrEqual(5, $number);
         $this->assertLessThanOrEqual(10, $number);
     }
 
-    public function testCantGetMinAndMaxRandomNumberBecauseMinIsGreaterThanMax() {
-        $this->setExpectedException("ArgumentOutOfRangeException");
+    public function test_Next_ThrowsExceptionWhenMinIsGreaterThanMax() {
+        # Arrange:
+        $this->setExpectedException("\\System\\ArgumentOutOfRangeException");
         $rnd = new Random();
+        
+        # Act:
         $number = $rnd->next(10, 1);
     }
 
-    public function testCanGetDoubleRandomNumber() {
+    public function test_NextDouble_CanGenerateRandomNumberBetweenZeroAndOne() {
+        # Arrange:
         $rnd = new Random();
+        
+        # Act:
         $number = $rnd->nextDouble();
+
+        # Assert:
         $this->assertGreaterThanOrEqual(0.0, $number);
         $this->assertLessThanOrEqual(1.0, $number);
     }
 
-    public function testCantGetBytesRandomNumbersBecauseBufferIsNull() {
-        $this->setExpectedException("ArgumentNullException");
+    public function test_NextBytes_ThrowsExceptionWhenBufferIsNull() {
+        # Arrange:
+        $this->setExpectedException("\\System\\ArgumentNullException");
         $rnd = new Random();
+
+        # Act:
         $rnd->nextBytes($result);
     }
 
-    public function testCanGetBytesRandomNumbers() {
-        $bytes = array(255, 1, 3, 255);
+    public function test_NextBytes_CanGenerateRandomNumbersToArray() {
+        # Arrange:
+        $bytes = array(-1, -1, -1, -1);
         $rnd = new Random();
+        
+        # Act:
         $rnd->nextBytes($bytes);
 
+        # Assert:
         for($i = 0; $i < sizeof($bytes); $i++) {
             $this->assertGreaterThanOrEqual(0, $bytes[$i]);
             $this->assertLessThanOrEqual(255, $bytes[$i]);
         }
     }
 }
-?>
